6bf5c3a84a2c334e1d0efa3423ea3fddf119669c
Nickcdub <61808212+Nickcdub@users.noreply.github.com>
2022-03-09 12:13:14 -0800

    Add files via upload

---
+++ sir-model/src/SIR.java
@@ -0,0 +1,42 @@
+
+
+public class SIR {
+
+    private final int population;
+    private final int vaccinated;
+    private final double rConstant;
+    private final double iConstant;
+    private final double infected;
+
+    protected SIR(int population, double transRate, double recovery, int vaccinated, int infected) {
+        this.population = population;
+        this.vaccinated = vaccinated;
+        this.infected = infected;
+
+        this.rConstant =  1/recovery;
+        this.iConstant = (transRate * rConstant) / (population-infected-(vaccinated*0.95));
+
+
+    }
+
+    public String getInfo(int days){
+        double[] R = new double[days+1];
+        R[0] = Math.ceil(vaccinated*0.95);
+
+        double[] I = new double[days+1];
+        I[0] = infected;
+
+        double[] S = new double[days+1];
+        S[0] = (population - R[0] - I[0]);
+
+        for (int i = 1; i <= days; i++) {
+            S[i] = S[i - 1] - (iConstant * I[i - 1] * S[i - 1]);
+            I[i] = I[i-1] + (iConstant*I[i-1]*S[i-1]) - (rConstant*I[i-1]);
+            R[i] = R[i-1] + (rConstant*I[i-1]);
+        }
+        return("S="+Math.round(S[days])+" I="+Math.round(I[days])+" R="+Math.round(R[days]));
+
+    }
+
+
+}
---
+++ sir-model/src/SIRTest.java
@@ -0,0 +1,9 @@
+
+public class SIRTest {
+
+    public static void main(String[] args) {
+
+        SIR Washington = new SIR(7766925,1.33,14, 32055,1784);
+        System.out.println(Washington.getInfo(200));
+    }
+}
