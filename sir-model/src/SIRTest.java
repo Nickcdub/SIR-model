9ea42e7d5e11375e08aa831b426d09b396a6a355
Nickcdub <61808212+Nickcdub@users.noreply.github.com>
2022-03-09 12:30:17 -0800

    Update SIRTest.java

--- sir-model/src/SIRTest.java
+++ sir-model/src/SIRTest.java
@@ -3,7 +3,7 @@ public class SIRTest {
 
     public static void main(String[] args) {
 
-        SIR Washington = new SIR(7766925,1.33,14, 32055,1784);
+        SIR Washington = new SIR(7766925,1.28,14, 5460000,1784);
         System.out.println(Washington.getInfo(200));
     }
 }
